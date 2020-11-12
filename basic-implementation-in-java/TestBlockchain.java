5dd62cabe4b9e6d8c1e65e3fef7135a8fb09006a
ramesh-11 <59366319+ramesh-11@users.noreply.github.com>
2020-11-12 17:13:31 +0530

    Add files via upload

---
+++ basic-implementation-in-java/TestBlockchain.java
@@ -0,0 +1,23 @@
+package project.blockchain;
+
+public class TestBlockchain {
+	public static void main(String args[]) {
+		
+		Blockchain bitCoin = new Blockchain();
+		
+		Block a = new Block("0x200", new java.util.Date(), "<transactions>");
+		Block b = new Block("0x200", new java.util.Date(), "<transactions>");
+		Block c = new Block("0x200", new java.util.Date(), "<transactions>");
+		
+		bitCoin.addBlock(a);
+		bitCoin.addBlock(b);
+		bitCoin.addBlock(c);
+		
+		//bitCoin.getLatestBlock().setPreviousHash("ABCDEFG");
+		
+		bitCoin.displayChain();
+		
+		bitCoin.isValid();
+
+	}
+}
